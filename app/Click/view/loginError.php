71804a20fc308ba601a5acc8675c207a59a17c13
MessasKouseila <k_messas@yahoo.fr>
2016-11-23 10:04:21 +0100

    showmessage

--- app/Click/view/loginError.php
+++ app/Click/view/loginError.php
@@ -6,7 +6,7 @@
                 <div class="account-wall">
                     <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
                          alt="">
-                    <form class="form-signin" action="Click.php?action=login">
+                    <form class="form-signin" action="">
                         <input type="text" name="login" class="form-control" placeholder="Login" required autofocus>
                         <input type="password" name="passWord" class="form-control" placeholder="Password" required>
                         <button class="btn btn-lg btn-primary btn-block" type="submit">
