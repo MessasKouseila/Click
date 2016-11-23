1495f39117cbce5e1706a023970d3fe470105da1
MessasKouseila <k_messas@yahoo.fr>
2016-11-23 21:52:43 +0100

    ViewsLogin

--- app/Click/view/loginError.php
+++ app/Click/view/loginError.php
@@ -1,23 +1,20 @@
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-6 col-md-4 col-md-offset-4">
-                <h1 class="text-center login-title">Veuillez vous authentifier</h1>
-                <div class="account-wall">
-                    <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                         alt="">
-                    <form class="form-signin" method="post" action="">
-                        <input type="text" name="login" class="form-control" placeholder="Login" required autofocus>
-                        <input type="password" name="passWord" class="form-control" placeholder="Password" required>
-                        <button class="btn btn-lg btn-primary btn-block" type="submit">
-                            Connexion</button>
-                        <label class="checkbox pull-left">
-                            <input type="checkbox" value="remember-me">
-                            se souvenir de moi
-                        </label>
-                    </form>
-                </div>
-                <a href="#" class="text-center new-account"> Inscription </a>
-            </div>
+<div class="row text-center">
+    <div class="col-sm-6 col-md-4 col-md-offset-4">
+        <h1 class="text-center login-title">Veuillez vous authentifier</h1>
+        <div class="account-wall">
+            <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+                 alt="">
+            <form class="form-signin" method="post" action="">
+                <input type="text" name="login" class="form-control" placeholder="Login" required autofocus>
+                <input type="password" name="passWord" class="form-control" placeholder="Password" required>
+                <button class="btn btn-lg btn-primary btn-block" type="submit">
+                    Connexion</button>
+                <label class="checkbox pull-left">
+                    <input type="checkbox" value="remember-me">
+                    se souvenir de moi
+                </label>
+            </form>
         </div>
+        <a href="#" class="text-center new-account"> Inscription </a>
     </div>
+</div>
