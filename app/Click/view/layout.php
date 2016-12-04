e1f56685cd8fb4e2b7c5179e68e83558421d41b0
MessasKouseila <k_messas@yahoo.fr>
2016-12-04 23:24:42 +0100

    le chat est draggable

--- app/Click/view/layout.php
+++ app/Click/view/layout.php
@@ -2,9 +2,10 @@
   <title>Click -</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <script src="js/jquery.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery-ui.min.js"></script>
   <style>
     /* Set black background color, white text and some padding */
     footer {
