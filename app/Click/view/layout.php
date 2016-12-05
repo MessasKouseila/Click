25f271698fb67d99bf0a084f4495d148ea92314b
MessasKouseila <k_messas@yahoo.fr>
2016-12-05 20:57:42 +0100

    ajout d'un css generale

--- app/Click/view/layout.php
+++ app/Click/view/layout.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/chat.css">
+    <link rel="stylesheet" href="css/general.css">
     <script src="js/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery-ui.min.js"></script>
