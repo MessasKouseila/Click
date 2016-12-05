ae2dacc355ab546cfb5df5a85adafe53b94c3b2f
MessasKouseila <k_messas@yahoo.fr>
2016-12-05 20:56:18 +0100

    ajout d'un css generale

--- app/Click/view/layout.php
+++ app/Click/view/layout.php
@@ -1,86 +1,90 @@
-<html><head>
-  <title>Click -</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/chat.css">
-  <script src="js/jquery.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/jquery-ui.min.js"></script>
-  <style>
-    /* Set black background color, white text and some padding */
-    footer {
-      background-color: #555;
-      color: white;
-      padding: 15px;
-    }
-  </style>
-</head><body>
+<html>
+<head>
+    <title>Click -</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/chat.css">
+    <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <style>
+        /* Set black background color, white text and some padding */
+        footer {
+            background-color: #555;
+            color: white;
+            padding: 15px;
+        }
+    </style>
+</head>
+<body>
 <nav class="navbar navbar-inverse" id="menu">
-  <div class="container-fluid" >
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Logo</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active">
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">Messages</a>
-        </li>
-      </ul>
-      <form class="navbar-form navbar-right" role="search">
-        <div class="form-group input-group">
-          <input type="text" class="form-control" placeholder="Search..">
-          <span class="input-group-btn">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active">
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Messages</a>
+                </li>
+            </ul>
+            <form class="navbar-form navbar-right" role="search">
+                <div class="form-group input-group">
+                    <input type="text" class="form-control" placeholder="Search..">
+                    <span class="input-group-btn">
                 <button class="btn btn-default" type="button">
                   <span class="glyphicon glyphicon-search"></span>
                 </button>
               </span>
+                </div>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a>
+                </li>
+            </ul>
         </div>
-      </form>
-      <ul class="nav navbar-nav navbar-right">
-        <li>
-          <a href="#"><span class="glyphicon glyphicon-user"></span> My Account</a>
-        </li>
-      </ul>
     </div>
-  </div>
 </nav>
-<div class="container-fluid text-center" id="mainWindow">
-  <div class="col-sm-3">
-    <div class="well" id="profil">
-      <?php include($template_view["profil"]); ?>
-    </div>
-    <div id="parentChat">
-        <div class="row" id="chat">
-          <?php include($template_view["chat"]); ?>
-          <script src="js/chat.js"></script>
+<div class="container-fluid text-center">
+    <div class="row">
+        <div class="col-sm-3">
+            <div class="well" id="profil">
+                <?php include($template_view["profil"]); ?>
+            </div>
+            <div id="parentChat">
+                <div class="row" id="chat">
+                    <?php include($template_view["chat"]); ?>
+                    <script src="js/chat.js"></script>
+                </div>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="row">
+                <div class="col-sm-12" id="statut">
+                    <?php include($template_view["statut"]); ?>
+                </div>
+            </div>
+            <?php include($template_view["mur"]); ?>
+
+        </div>
+        <div class="col-sm-3" id="listeUser">
+            <?php include($template_view["listeUsers"]); ?>
         </div>
     </div>
-  </div>
-    <div class="col-sm-7">
-      <div class="row">
-        <div class="col-sm-12" id="statut">
-          <?php include($template_view["statut"]); ?>
-        </div>
-      </div>
-      <?php include($template_view["mur"]); ?>
+</div>
+<footer class="section section-primary">
 
-    </div>
-    <div class="col-sm-2" id="listeUser">
-      <?php include($template_view["listeUsers"]); ?>
-    </div>
-  </div>
-  <footer class="section section-primary">
-
-  </footer>
+</footer>
 
 </body>
 </html>
