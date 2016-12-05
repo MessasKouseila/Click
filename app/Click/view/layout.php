6a9a20002ae0ac9fe8759058d706b9eef4079161
MessasKouseila <k_messas@yahoo.fr>
2016-12-05 20:47:52 +0100

    inclusion du chat.css

--- app/Click/view/layout.php
+++ app/Click/view/layout.php
@@ -3,6 +3,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/chat.css">
   <script src="js/jquery.js"></script>
   <script src="js/bootstrap.min.js"></script>
   <script src="js/jquery-ui.min.js"></script>
