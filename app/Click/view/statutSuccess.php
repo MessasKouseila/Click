eade8771b5df6122b3a1e3f8a85e0c8f8e07ac89
MessasKouseila <k_messas@yahoo.fr>
2016-12-06 22:02:07 +0100

    ajout du php à la vue chat

--- app/Click/view/statutSuccess.php
+++ app/Click/view/statutSuccess.php
@@ -1,5 +1,5 @@
-<div class="panel panel-default text-left">
-    <div class="panel-body">
+<div class="panel panel-default text-left" >
+    <div class="panel-body" style="overflow: hidden;">
         <p contenteditable="true">Status: <?php $context->user->statut ?></p>
         <button type="button" class="btn btn-default btn-sm">Modifier</button>
     </div>
